b54370c75fc55ecbd42a74f0ce870e24b08c997d
Kamil Patel <71028182+Kamil1047@users.noreply.github.com>
2021-10-19 16:13:05 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,17 +2,8 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBzRLIwDXe-Vwa4_xY-Cy5UOlk2tGQIw98",
-
-  authDomain: "crud-firebase-react-3db21.firebaseapp.com",
-
-  projectId: "crud-firebase-react-3db21",
-
-  storageBucket: "crud-firebase-react-3db21.appspot.com",
-
-  messagingSenderId: "367009431703",
-
-  appId: "1:367009431703:web:c2d215366794aa366469d2",
+ 
+  
 };
 
 const app = initializeApp(firebaseConfig);
